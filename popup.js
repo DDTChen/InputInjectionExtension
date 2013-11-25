11c147bffbc1e09827b72b2b1a1dcaec9ab4d4e8
DDTChen <ddttchen@gmail.com>
2013-11-25 21:06:18 +0800

    使bootstrap v3的button也可以呼叫chrome api

--- popup.js
+++ popup.js
@@ -7,14 +7,11 @@ function click(e) {
     chrome.tabs.executeScript(null,
         {code:"document.body.style.backgroundColor='" + e.target.id + "'"});
 
-    var javascript = "document.getElementById('lst-ib').value='11';"
+    var javascript = "document.getElementById('lst-ib').value='" + e.target.id + "';"
     chrome.tabs.executeScript(null, {code:javascript});
     window.close();
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    var divs = document.querySelectorAll('div');
-    for (var i = 0; i < divs.length; i++) {
-        divs[i].addEventListener('click', click);
-    }
+    $('.btn2').bind('click', click);
 });
